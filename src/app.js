b0a944b458cab829a4b8551046b7eead63775d03
YuvalShaybak8 <95643357+YuvalShaybak8@users.noreply.github.com>
2022-09-06 19:25:16 +0300

    initial-commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+//create express app and listen on post 3001
+const express = require('express');
+const bp=require('body-parser');
+const app = express();
+
+app.use(bp.urlencoded({extended:true}));
+app.use(bp.json());
+
+app.use(express.static('public'));
+app.set("view engine", "ejs");
+
+app.set('views', __dirname + '/views');
+
+app.get('/', (req, res) => res.render('index'));
+
+app.listen(3000, () => console.log(`Example app listening on port 3000!`));
