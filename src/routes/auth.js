e4d112e2dda0825585366580018eca6bcee9d549
May Shech <may745@gmail.com>
2022-09-11 00:28:22 +0300

    working perfect

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/user");
 const CryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
 
 // REGISTER
@@ -9,7 +10,8 @@ router.post("/register", async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC)
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC),
+        isAdmin: req.body.isAdmin
     });
     try {
         const savedUser = await newUser.save();
@@ -27,32 +29,29 @@ router.post("/login", async (req, res) => {
         const user = await User.findOne({
             username: req.body.username
         });
-        if(user.username != req.body.username){ 
+        if(!user){ 
             res.status(401).json("Wrong credentials!");
             return;
         };
-        if(user.email != req.body.email) {
-            res.status(401).json("Wrong credentials!");
-            return;
-        };
-        console.log(user.username);
-        console.log(user.email);
-        console.log(user.password);
-
 
         const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC);
 
         const Originalpassword = hashedPassword.toString(CryptoJS.enc.Utf8);
-        console.log(Originalpassword);
 
         if(Originalpassword != req.body.password){
-            res.status(401).json("Wrong credentials!");
+            res.status(401).json("incorrect password!");
             return;
         }
 
-        const { password, ...others } = user._doc;
+        const accessToken = jwt.sign({
+            id:user.id,
+            isAdmin: user.isAdmin,
+        },  process.env.JWT_SEC,
+        {expiresIn:"3d"}
+        );
 
-        res.status(200).json(others);
+        const { password, ...others } = user._doc;
+        res.status(200).json({...others, accessToken});
     } catch (err) {
         res.status(500).json(err);
     }
