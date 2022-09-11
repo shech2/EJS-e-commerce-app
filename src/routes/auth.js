fb2275913d6ae10e5f84d0389fb86ab6b0b6f0e5
YuvalShaybak8 <yuvalsaybak@gmail.com>
2022-09-11 12:48:01 +0300

    test4

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,14 +1,10 @@
 const router = require("express").Router();
 const controller = require("../controllers/auth");
 
-
-
 // REGISTER
 router.post("/register",controller.auth_RegController);
 
-
 // LOGIN
 router.post("/login",controller.auth_LogController);
-
 
 module.exports = router;
