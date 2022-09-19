7e7744fa108e80b49d746ec82176755e33a4ee5c
Sassy16 <giladbadin121@cs.colman.ac.il>
2022-09-19 11:48:52 +0300

    :))

--- src/app.js
+++ src/app.js
@@ -104,7 +104,6 @@ app.post('/login' , authRouter);
 // Main Route:
 app.get('/', (req, res) => res.render('index'));
 
-app.listen(3000, () => console.log(`Example app listening on port 3000!`));
 
 // ROUTES:
 app.use("/api/",ProductRouter); 
