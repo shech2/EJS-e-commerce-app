ebbb7d8280726fcddaa5dd04c6796ff43dc6c023
YuvalShaybak8 <yuvalsaybak@cs.colman.ac.il>
2022-09-22 12:28:06 +0300

    order change

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -3,7 +3,9 @@ const router = require("express").Router();
 const OrderItem = require("../models/order-item");
 
 router.get("/", async (req, res) => {
-  const orderList = await Order.find().populate("user", "name").sort({ dateOrdered: -1 });// -1 it means descending order
+  const orderList = await Order.find()
+    .populate("user", "name")
+    .sort({ dateOrdered: -1 }); // -1 it means descending order
 
   if (!orderList) {
     res.status(500).json({ success: false });
@@ -13,10 +15,13 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   const order = await Order.findById(req.params.id)
-  .populate("user", "name")
-  .populate({
-    path: "orderItems", populate: {
-      path: "product", populate: "category"}
+    .populate("user", "name")
+    .populate({
+      path: "orderItems",
+      populate: {
+        path: "product",
+        populate: "category",
+      },
     });
 
   if (!order) {
@@ -41,22 +46,20 @@ router.put("/order/:id", async (req, res) => {
 });
 
 
-
-
-
 //create the orders id
 router.post("/", async (req, res) => {
-  const orderItemsIds = Promise.all(req.body.orderItems.map(async orderItem => {
-    let newOrderItem = new OrderItem({
-      quantity: orderItem.quantity,
-      product: orderItem.product
+  const orderItemsIds = Promise.all(
+    req.body.orderItems.map(async (orderItem) => {
+      let newOrderItem = new OrderItem({
+        quantity: orderItem.quantity,
+        product: orderItem.product,
+      });
+
+      newOrderItem = await newOrderItem.save();
+
+      return newOrderItem._id;
     })
-
-    newOrderItem = await newOrderItem.save();
-
-    return newOrderItem._id;
- 
-  }))
+  );
 
   const orderItemsIdsResolved = await orderItemsIds;
 
@@ -75,11 +78,27 @@ router.post("/", async (req, res) => {
   });
   order = await order.save();
 
-  if (!order) 
-  return res.status(400).send("the order cannot be created!");
+  if (!order) return res.status(400).send("the order cannot be created!");
 
   res.send(order);
 });
 
+//delete order
+router.delete("/order/:id", (req, res) => {
+  Order.findByIdAndRemove(req.params.id).then(async order => {
+      if (order) {
+        await order.orderItems.map(async orderItem => {
+          await OrderItem.findByIdAndRemove(orderItem);
+        });
+        return res
+          .status(200).json({ success: true, message: "the order is deleted!" });
+      } else {
+        return res.status(404).json({ success: false, message: "order not found!" });
+      }
+    })
+    .catch((err) => {
+      return res.status(400).json({ success: false, error: err });
+    });
+});
 
 module.exports = router;
