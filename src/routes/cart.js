987a2991ca371fd6b056e1adec6c6c0466c1c614
YuvalShaybak8 <yuvalsaybak@cs.colman.ac.il>
2022-10-12 12:10:44 +0300

    Merge branch main

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -2,19 +2,14 @@ const router = require("express").Router();
 const Cart = require("../models/cart");
 const product = require("../models/Product");
 
-//create 
-
-router.post("/", verifyToken, async (req, res) => {
-    const newCart = new Cart(req.body);
+//create cart
 
 router.post("/add-to-cart", async (req, res) => {
     const AddedProduct = await product.findById(req.body.productId);
     if (!AddedProduct) return res.status(400).send("Product not found");
 
-
     Cart.save(AddedProduct);
     console.log(Cart.getCart());
 });
-
 
 module.exports = router;
