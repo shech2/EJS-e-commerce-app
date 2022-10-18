d0c8dc0dccd1fd90a5c525edfb73e1ebcd4867dd
May Shech <may745@gmail.com>
2022-10-18 14:10:06 +0300

    ..

--- src/middleware/authMiddleWare.js
+++ src/middleware/authMiddleWare.js
@@ -3,12 +3,14 @@ const jwt = require("jsonwebtoken");
 
 exports.authMiddleware = (req, res, next) => {
     const token = req.cookies.jwt;
+    // const params = new URLSearchParams(window.location.search)
     if(token){
         jwt.verify(token, process.env.JWT_SEC, (err, decodedToken) => {
             if(err){
                 console.log(err.message);
                 res.redirect("/login");
             } else {
+                // req.params = params;
                 req.user = decodedToken;
                 next();
             }
@@ -24,6 +26,7 @@ exports.authMiddleware = (req, res, next) => {
 exports.authAdmin = (req, res, next) => {
     const token = req.cookies.jwt;
     const isAdmin = req.cookies.isAdmin;
+    // const params = new URLSearchParams(window.location.search)
     if(token && isAdmin){
         jwt.verify(token, process.env.JWT_SEC, (err, decodedToken) => {
             if(err){
@@ -31,6 +34,7 @@ exports.authAdmin = (req, res, next) => {
                 res.redirect("/login");
             } else {
                 if(decodedToken.isAdmin){
+                    // req.params = params;
                     req.user = decodedToken;
                     next();
                 } else {
