b996d91430e1f439d5c6baf7044e697b0486f332
May Shech <may745@gmail.com>
2022-10-29 00:18:09 +0300

    s

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -6,5 +6,8 @@ router.post('/add-to-cart', authmw.authMiddleware , CartController.addItemToCart
 
 router.post('/remove-from-cart', authmw.authMiddleware , CartController.RemoveFromCart);
 
+// router.post('/remove-all', authmw.authMiddleware , CartController.removeAll);
+
+
 
 module.exports = router;
