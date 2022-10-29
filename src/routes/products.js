8d878f384d284e5c0e94693502d8167c0fc2ce15
May Shech <may745@gmail.com>
2022-10-29 16:23:25 +0300

    Facebook api is working but need to fix something

--- src/routes/products.js
+++ src/routes/products.js
@@ -6,17 +6,16 @@ const mongoose = require("mongoose");
 
 router.get("/products", async (req, res) => {
     let filter = {};
-    
-    if(req.query.category)
-    {
-        filter = {category:req.query.categories.split(",")}
+
+    if (req.query.category) {
+        filter = { category: req.query.categories.split(",") }
     }
-        
-        
+
+
     const productList = await Product.find(filter).populate('category'); // filter array of categories
 
-    if(!productList) {
-        res.status(500).json({success: false})
+    if (!productList) {
+        res.status(500).json({ success: false })
     }
     res.send(productList);
 })
@@ -24,90 +23,89 @@ router.get("/products", async (req, res) => {
 router.get("/products/:id", async (req, res) => {
     const product = await Product.findById(req.params.id).populate('category');
 
-    if(!product) {
-        res.status(500).json({success: false})
+    if (!product) {
+        res.status(500).json({ success: false })
     }
     res.send(product);
 })
 
 //Create a new product
-router.post("/create_product", async (req,res) => {
+router.post("/create_product", async (req, res) => {
     const category = await Category.findById(req.body.category);
-    if(!category) return res.status(400).send("Invalid Category");
+    if (!category) return res.status(400).send("Invalid Category");
 
     const product = new Product({
         Product_name: req.body.Product_Name,
         price: req.body.price,
         description: req.body.description,
         category: req.body.category,
-        size:[{ size : req.body.size,sizeQuantity : req.body.quantity2}],
+        size: [{ size: req.body.size, sizeQuantity: req.body.quantity2 }],
         brand: req.body.brand,
         quantity: req.body.quantity,
         rating: req.body.rating,
         image: req.body.image,
     });
 
-    console.log(product);
 
-    
     product.save().then((createdProduct => {
-        res.redirect("/shop");
+        // res.redirect("/shop");
+        res.status(201).json(createdProduct);
     })).catch((err) => {
         res.status(500).json({
             error: err,
             success: false
         })
-    })
+    });
 });
 
 //update product
 router.put("/products/:id", async (req, res) => {
     //validate id check
-    if(mongoose.isValidObjectId(req.params.id)){
+    if (mongoose.isValidObjectId(req.params.id)) {
         res.status(400).send("Invalid Product Id");
-    } 
+    }
     const category = await Category.findById(req.body.category);
-    if(!category) return res.status(400).send("Invalid Category");
+    if (!category) return res.status(400).send("Invalid Category");
 
-  const product = await Product.findByIdAndUpdate(
-    req.params.id,
-    {
-      Product_name: req.body.Product_Name,
-      price: req.body.price,
-      description: req.body.description,
-      category: req.body.category,
-      size:[{ size : req.body.size,sizeQuantity : req.body.quantity2}],
-      brand: req.body.brand,
-      quantity: req.body.quantity,
-      rating: req.body.rating,
-      image: req.body.image,
-    },
-    { new: true }
-  );
+    const product = await Product.findByIdAndUpdate(
+        req.params.id,
+        {
+            Product_name: req.body.Product_Name,
+            price: req.body.price,
+            description: req.body.description,
+            category: req.body.category,
+            size: [{ size: req.body.size, sizeQuantity: req.body.quantity2 }],
+            brand: req.body.brand,
+            quantity: req.body.quantity,
+            rating: req.body.rating,
+            image: req.body.image,
+        },
+        { new: true }
+    );
 
-  if (!product) {
-    return res.status(500).send("The product cannot be updated");
-  }
+    if (!product) {
+        return res.status(500).send("The product cannot be updated");
+    }
 });
 
 //Delete product
 router.delete("/products/:id", (req, res) => {
     Product.findByIdAndRemove(req.params.id).then(product => {
-        if(product) {
-            return res.status(200).json({success: true, message: "the product is deleted!"})
+        if (product) {
+            return res.status(200).json({ success: true, message: "the product is deleted!" })
         } else {
-            return res.status(404).json({success: false, message: "product not found!"})
+            return res.status(404).json({ success: false, message: "product not found!" })
         }
     }).catch(err => {
-        return res.status(400).json({success: false, error: err})
+        return res.status(400).json({ success: false, error: err })
     })
 })
 
 router.get("/get/count", async (req, res) => {
     const productCount = await Product.countDocuments((count) => count);
 
-    if(!productCount) {
-        res.status(500).json({success: false})
+    if (!productCount) {
+        res.status(500).json({ success: false })
     }
     res.send({
         productCount: productCount
