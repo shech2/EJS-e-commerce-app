d55c0af37e0cf5288ae33d5751be34eee1f96720
May Shech <may745@gmail.com>
2022-11-11 19:00:48 +0200

    Stripe

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -1,6 +1,9 @@
 const CartController = require('../models/cart');
 const product = require('../models/Product');
 
+// This is your test secret API key.
+const stripe = require("stripe")('sk_test_51M1TVWH8e0iQz01ZXwnTrube3wnQ9P1e98dTjCEwFK8Xx8YHKNeR9YOiEOj1bGVPPQybmWpMFra44dzjmMwH4nob00NDoAE71e');
+
 
 // Add item to cart
 exports.addItemToCart = async (req, res) => {
@@ -56,24 +59,24 @@ exports.addItemToCart = async (req, res) => {
 exports.RemoveFromCart = async (req, res) => {
     const productId = req.body.productId;
     const size = req.body.size;
-    
+
 
     CartController.findOneAndUpdate(
         { user: req.user.id },
         {
-          $pull: {
-              cartItems: {
-                product: productId,
-                size
-              },
-          },
+            $pull: {
+                cartItems: {
+                    product: productId,
+                    size
+                },
+            },
         }
-        ).exec((error, _cart) => {
-          if (error) return res.status(400).json({ error });
-          if (_cart) {
+    ).exec((error, _cart) => {
+        if (error) return res.status(400).json({ error });
+        if (_cart) {
             return res.redirect("/cart");
-          }
-        });
+        }
+    });
 
 }
 
@@ -95,37 +98,37 @@ exports.removeAll = async (req, res) => {
 
 // Update quantity of item in cart
 exports.updateQuantity = async (req, res) => {
-  const productId = req.body.productId;
-  const quantity = req.body.quantity;
-  const size = req.body.size;
+    const productId = req.body.productId;
+    const quantity = req.body.quantity;
+    const size = req.body.size;
 
-  const productPOST = await product.findById(productId);
+    const productPOST = await product.findById(productId);
 
-  if (quantity > 0) {
-    if (product) {
-      CartController.findOneAndUpdate(
-        { user: req.user.id, "cartItems.product": productPOST.id , "cartItems.size": size},
-        {
-          $set: {
-            "cartItems.$": {
-              product: productPOST.id,
-              quantity: quantity,
-              price: productPOST.price,
-              brand: productPOST.brand,
-              size: size,
-            },
-          },
+    if (quantity > 0) {
+        if (product) {
+            CartController.findOneAndUpdate(
+                { user: req.user.id, "cartItems.product": productPOST.id, "cartItems.size": size },
+                {
+                    $set: {
+                        "cartItems.$": {
+                            product: productPOST.id,
+                            quantity: quantity,
+                            price: productPOST.price,
+                            brand: productPOST.brand,
+                            size: size,
+                        },
+                    },
+                }
+            ).exec((error, _cart) => {
+                if (error) return res.status(400).json({ error });
+                if (_cart) {
+                    res.status(201).json({ _cart });
+                }
+            });
         }
-      ).exec((error, _cart) => {
-        if (error) return res.status(400).json({ error });
-        if (_cart) {
-          res.status(201).json({ _cart });
-        }
-      });
+    } else {
+        return res.status(400).json({ error: "Quantity cannot be less than 1" });
     }
-  } else {
-    return res.status(400).json({ error: "Quantity cannot be less than 1" });
-  }
 };
 
 // Add size to an Item in the cart
@@ -133,13 +136,13 @@ exports.addSizeToCart = async (req, res) => {
     const POSTProduct = await product.findById(req.body.productId);
     const quantity = req.body.quantity;
     const size = req.body.size;
-    if(size=="Select Size"){
-        return res.status(500).json({message: "Error! choose size"});
+    if (size == "Select Size") {
+        return res.status(500).json({ message: "Error! choose size" });
     }
     product.findOneAndUpdate({ _id: POSTProduct.id }, {
         $set: {
-            "size.size" : size
-        } 
+            "size.size": size
+        }
     }).exec();
 
     CartController.findOne({ user: req.user.id }).exec((error, cart) => {
@@ -148,30 +151,30 @@ exports.addSizeToCart = async (req, res) => {
             const product = POSTProduct;
 
             let exist = false;
-            for(let i=0;i<cart.cartItems.length;i++){
-                if(cart.cartItems[i].product.toString()==product.id.toString() && cart.cartItems[i].size==size){
+            for (let i = 0; i < cart.cartItems.length; i++) {
+                if (cart.cartItems[i].product.toString() == product.id.toString() && cart.cartItems[i].size == size) {
                     exist = true;
-                    cart.cartItems[i].quantity+=1;
+                    cart.cartItems[i].quantity += 1;
                     break;
                 }
             }
             exist
-              ? cart.save()
-              : cart
-                  .updateOne({
-                    $push: {
-                      cartItems: {
-                        product: product.id,
-                        quantity: quantity,
-                        price: product.price,
-                        brand: product.brand,
-                        size: size,
-                      },
-                    },
-                  })
-                  .exec(() => res.status(201).json({ cart }));
-            
-        }else{
+                ? cart.save()
+                : cart
+                    .updateOne({
+                        $push: {
+                            cartItems: {
+                                product: product.id,
+                                quantity: quantity,
+                                price: product.price,
+                                brand: product.brand,
+                                size: size,
+                            },
+                        },
+                    })
+                    .exec(() => res.status(201).json({ cart }));
+
+        } else {
             console.log('no cart found')
         }
     })
@@ -182,14 +185,50 @@ exports.updateSizeArray = async (req, res) => {
     const cart = await CartController.findOne({ user: req.user.id }).populate('cartItems.product', '_id name price brand quantity size').exec();
     // for each item in cart call update function
     cart.cartItems.forEach(async (item) => {
-    const sizeCart = item.product.size.size;
-    item.product.size.sizeArray = item.product.size.sizeArray.filter((item) => item != sizeCart);
-    console.log(item.product.size.sizeArray);
-    product.findOneAndUpdate({ _id: item.product._id }, {
+        const sizeCart = item.product.size.size;
+        item.product.size.sizeArray = item.product.size.sizeArray.filter((item) => item != sizeCart);
+        console.log(item.product.size.sizeArray);
+        product.findOneAndUpdate({ _id: item.product._id }, {
             $set: {
-                "size.sizeArray" : item.product.size.sizeArray
+                "size.sizeArray": item.product.size.sizeArray
             }
         }).exec();
     });
     res.status(200).json({ cart });
+}
+
+
+
+exports.checkout = async (req, res) => {
+    const cart = await CartController.findOne({ user: req.user.id }).populate('cartItems.product', '_id name price brand quantity size').exec();
+
+    let products = [];
+    let totalAmount = 0;
+    let tax = 0.17;
+    cart.cartItems.forEach((item) => {
+        products.push({
+            product: item.product._id,
+            quantity: item.quantity,
+            price: item.price,
+            brand: item.brand,
+            size: item.size,
+        });
+        totalAmount = totalAmount + item.product.price * item.quantity;
+    });
+    totalAmount = totalAmount + totalAmount * tax;
+    // Create a PaymentIntent with the order amount and currency
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: parseInt(totalAmount),
+        currency: "usd",
+        automatic_payment_methods: {
+            enabled: true,
+        },
+
+
+    });
+
+
+    res.send({
+        clientSecret: paymentIntent.client_secret,
+    });
 }
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -2,16 +2,19 @@ const router = require("express").Router();
 const CartController = require("../controllers/cart");
 const authmw = require("../middleware/authMiddleware");
 
-router.post('/add-to-cart', authmw.authMiddleware , CartController.addItemToCart);
+router.post('/add-to-cart', authmw.authMiddleware, CartController.addItemToCart);
 
-router.post('/remove-from-cart', authmw.authMiddleware , CartController.RemoveFromCart);
+router.post('/remove-from-cart', authmw.authMiddleware, CartController.RemoveFromCart);
 
-router.post('/remove-all', authmw.authMiddleware , CartController.removeAll);
+router.post('/remove-all', authmw.authMiddleware, CartController.removeAll);
 
-router.post('/update-cart', authmw.authMiddleware , CartController.updateQuantity);
+router.post('/update-cart', authmw.authMiddleware, CartController.updateQuantity);
 
-router.post('/add-size-to-cart', authmw.authMiddleware , CartController.addSizeToCart);
+router.post('/add-size-to-cart', authmw.authMiddleware, CartController.addSizeToCart);
 
-router.post('/update-size-array', authmw.authMiddleware , CartController.updateSizeArray);
+router.post('/update-size-array', authmw.authMiddleware, CartController.updateSizeArray);
+
+router.post('/checkout', authmw.authMiddleware, CartController.checkout);
+
 
 module.exports = router;
